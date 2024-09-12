ea3f80bfcc301c4a45aeb1c1d5c350129866fdb6
Hadi Chokr <158838697+silverhadch@users.noreply.github.com>
2024-09-12 09:18:52 +0200

    Referenzen fürs printen
    Mehr Rust.

--- src/main.rs
+++ src/main.rs
@@ -1,30 +1,28 @@
 fn main() {
     let mut finish = false;
-    let mut array_index: usize = 0; // usize sind für Arrays optimiert
-    let mut switch: i32 = 0;
-    let mut init = true;
+    let mut array_index: usize = 0;
+    let mut switch: i32 = -1;
 
     let mut arr: [i32; 15] = [11, 5, 8, 5, 2, 9, 5, 88, 6, 557, 5588, 96, 35, 66, 4];
-    println!("Startarray: {:?}", arr);
+    println!("Start array: {:?}", &arr);
 
     while !finish {
-        let längearr = arr.len(); 
+        let length = arr.len();
 
-        if array_index < längearr - 1 && arr[array_index] > arr[array_index + 1] {  
+        if array_index < length - 1 && arr[array_index] > arr[array_index + 1] {  
             arr.swap(array_index, array_index + 1);
             switch += 1;
         }
-        
-        array_index += 1; 
-        if array_index >= längearr - 1 {
-            if switch == 0 && !init { 
-                finish = true; 
-                println!("Ergebnis: {:?}", arr);
+
+        array_index += 1;
+        if array_index >= length - 1 {
+            if switch == 0 {
+                finish = true;
+                println!("Sorted result: {:?}", &arr);
             }
-            
+
             array_index = 0;
             switch = 0;
-            init = false;
         }
     }
 }
